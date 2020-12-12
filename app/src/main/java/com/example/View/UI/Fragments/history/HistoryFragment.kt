b67a8173edc9a87b5959eb61c2a391c117627ffc
ahuaynap <ahuaynap@unsa.edu.pe>
2020-12-11 21:54:19 -0500

    History workout

--- app/src/main/java/com/example/View/UI/Fragments/history/HistoryFragment.kt
+++ app/src/main/java/com/example/View/UI/Fragments/history/HistoryFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -29,12 +30,13 @@ class HistoryFragment : Fragment() {
             ViewModelProvider(this).get(HistoryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_history, container, false)
         val textView: TextView = root.findViewById(R.id.text_history_title)
+
         historyViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
 
         //logout button
-        val logOutBtn: FloatingActionButton = root.findViewById(R.id.logOutButton)
+        val logOutBtn: ImageView = root.findViewById(R.id.logOutButton)
         logOutBtn.setOnClickListener {
             var loginViewModel: LoginViewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
             loginViewModel.logout()
