e0f095e7527e8700d19683469b77a03885fb1cc8
klebersin <kleber.sik@gmail.com>
2020-12-14 21:46:39 -0500

    locationCommit2

--- app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.ViewModel
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
 import com.example.dto.Coordinate
 import com.example.dto.Training
@@ -9,6 +11,8 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class WorkoutViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -20,14 +24,22 @@ class WorkoutViewModel(application: Application) : AndroidViewModel(application)
     private val locationLiveData = LocationLiveData(application)
     fun getLocationLiveData() = locationLiveData
     fun addCoordinate(trainingId: String,coordinate: Coordinate) {
-        reference.child("trainings").child(trainingId).child("coordinates").setValue(coordinate)
+        val id = reference.push().key
+        reference.child("trainings").child(trainingId).child("coordinates").child(id!!).setValue(coordinate)
     }
 
     fun createTraining(): Training {
         val id = reference.push().key
-        val training = Training(id!!,0,"0","run")
+        val training = Training(id!!,"00/00/0000",0,"0","run")
 
         reference.child("trainings").child(id).setValue(training)
         return training
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setData(trainingId: String, time: CharSequence?) {
+        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+        reference.child("trainings").child(trainingId).child("date").setValue(LocalDateTime.now().format(formatter))
+        reference.child("trainings").child(trainingId).child("time").setValue(time)
     }
 }
