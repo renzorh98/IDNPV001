fbd6d2122732c1f6b88df0e84da68c103b3fcbdd
klebersin <kleber.sik@gmail.com>
2020-12-15 22:04:55 -0500

    locationCommit3

--- app/src/main/java/com/example/View/UI/Fragments/workout/WorkoutFragment.kt
+++ app/src/main/java/com/example/View/UI/Fragments/workout/WorkoutFragment.kt
@@ -1,40 +1,38 @@
 package com.example.View.UI.Fragments.workout
 
+import android.Manifest
+import android.app.Activity
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.ViewModel.WorkoutViewModel
 import com.example.idnpv001.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import android.Manifest
-import android.annotation.SuppressLint
-import android.os.Build
-import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.lifecycle.Observer
-import com.example.dto.Training
-
-import com.google.android.gms.maps.model.PolylineOptions
 import kotlinx.android.synthetic.main.fragment_workout.*
+
 
 class WorkoutFragment : Fragment(), OnMapReadyCallback {
 
+
     private val LOCATION_PERMISSION_REQUEST_CODE = 2000
-    private lateinit var training: Training
     private lateinit var workoutViewModel: WorkoutViewModel
     private lateinit var mMap: GoogleMap
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -43,87 +41,34 @@ class WorkoutFragment : Fragment(), OnMapReadyCallback {
         workoutViewModel =
             ViewModelProvider(this).get(WorkoutViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_workout, container, false)
-        btn_start.setOnClickListener{
-            startTraining()
-        }
-        btn_stop.setOnClickListener {
-            stopTraining()
-        }
+
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        mapFragment.onCreate(arguments)
+        mapFragment.onCreate(this.arguments)
+
+
+        prepRequestLocationUpdates()
 
         return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-
-        super.onActivityCreated(savedInstanceState)
-        prepRequestLocationUpdates()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun stopTraining(){
-        workoutViewModel.setData(training.trainingId, chronometer.text)
-        chronometer.stop()
-
-    }
-    private fun startTraining(){
-
-        chronometer.start()
-        training = workoutViewModel.createTraining()
-    }
-
     private fun prepRequestLocationUpdates() {
-
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            requestLocationUpdates()
+        if (ContextCompat.checkSelfPermission(this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            workoutViewModel.getCurrentLocation()
         } else {
             val permissionRequest = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
             requestPermissions(permissionRequest, LOCATION_PERMISSION_REQUEST_CODE)
         }
     }
-    private fun requestLocationUpdates() {
-        workoutViewModel.getLocationLiveData().observe(viewLifecycleOwner, Observer {
-            workoutViewModel.addCoordinate(training.trainingId, it)
-            mMap.addPolyline(PolylineOptions()
-                .clickable(true)
-                .add(
-                    LatLng(it.latitude, it.longitude)
-                )
-            )
-        })
-    }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when(requestCode) {
-            LOCATION_PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==  PackageManager.PERMISSION_GRANTED) {
-                    requestLocationUpdates()
-                } else {
-                    Toast.makeText(context, "Unable to update location without permission", Toast.LENGTH_LONG).show()
-                }
-            }
-            else -> {
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-            }
-        }
-    }
 
-    @SuppressLint("MissingPermission")
+
     override fun onMapReady(googleMap: GoogleMap?) {
         if (googleMap != null) {
             mMap = googleMap
         }
-        mMap.isMyLocationEnabled = true
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
 }
--- app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
@@ -1,45 +1,36 @@
 package com.example.ViewModel
 
 import android.app.Application
-import android.os.Build
-import androidx.annotation.RequiresApi
+
+import android.location.Location
+
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import com.example.dto.Coordinate
-import com.example.dto.Training
-import com.example.liveData.LocationLiveData
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+
 
 class WorkoutViewModel(application: Application) : AndroidViewModel(application) {
 
+
+    private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(application)
     private var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+
     private var reference: DatabaseReference
+
     init {
         reference = database.getReference("idnpv001-default-rtdb")
     }
-    private val locationLiveData = LocationLiveData(application)
-    fun getLocationLiveData() = locationLiveData
-    fun addCoordinate(trainingId: String,coordinate: Coordinate) {
-        val id = reference.push().key
-        reference.child("trainings").child(trainingId).child("coordinates").child(id!!).setValue(coordinate)
-    }
 
-    fun createTraining(): Training {
-        val id = reference.push().key
-        val training = Training(id!!,"00/00/0000",0,"0","run")
 
-        reference.child("trainings").child(id).setValue(training)
-        return training
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun setData(trainingId: String, time: CharSequence?) {
-        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-        reference.child("trainings").child(trainingId).child("date").setValue(LocalDateTime.now().format(formatter))
-        reference.child("trainings").child(trainingId).child("time").setValue(time)
+    fun getCurrentLocation(){
+        var myPosition: LatLng
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener {
+                    location : Location ->
+                Log.e("Location", "Latitude" + location.latitude +" "+ location.longitude)
+            }
     }
 }
