75d410987880da04a180bcb96eec9636e4cc4d2e
klebersin <kleber.sik@gmail.com>
2020-12-16 16:43:49 -0500

    locationCommit5

--- app/src/main/java/com/example/Model/database/AppDatabase.kt
+++ app/src/main/java/com/example/Model/database/AppDatabase.kt
@@ -1,41 +1,13 @@
 package com.example.Model.database
 
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import com.example.Model.DAO.TrainingDao
-import com.example.Model.DAO.TrajectoryDao
-import com.example.Model.DAO.UserDao
-import com.example.Model.Entities.Training
-import com.example.Model.Entities.Trajectory
-import com.example.Model.Entities.User
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
+class AppDatabase {
 
-@Database(entities = [User:: class, Trajectory:: class, Training::class], version = 1)
-abstract class AppDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
-    abstract fun trajectoryDao(): TrajectoryDao
-    abstract fun trainingDao(): TrainingDao
-
-    companion object{
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            val tempInstance = INSTANCE
-            if(tempInstance != null){
-                return tempInstance
-            }
-            synchronized(this) {
-                val db = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java, "app_database"
-                ).build()
-                INSTANCE = db
-                return db
-            }
-        }
+    private val myRef = Firebase.database.reference
+    fun getReference(): DatabaseReference {
+        return myRef
     }
-
 }
--- app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
+++ app/src/main/java/com/example/ViewModel/WorkoutViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.ViewModel
 
 import android.app.Application
-import android.location.Location
+import android.os.Build
+import androidx.annotation.RequiresApi
+
 import androidx.lifecycle.AndroidViewModel
+import com.example.Model.database.AppDatabase
 import com.example.View.UI.Fragments.workout.LocationLiveData
 import com.example.dto.Coordinate
+import com.example.dto.Training
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DatabaseReference
+import java.time.LocalDateTime
 
 
 class WorkoutViewModel(application: Application) : AndroidViewModel(application) {
@@ -16,17 +22,40 @@ class WorkoutViewModel(application: Application) : AndroidViewModel(application)
     private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(application)
     private val locationLiveData = LocationLiveData(application)
     internal fun getLocationLiveData() = locationLiveData
-
-
-    fun getCurrentLocation(mMap: GoogleMap){
-
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener {
-                mMap.addMarker(
-                    MarkerOptions()
-                        .position(LatLng(it.latitude, it.longitude))
-                        .title("My position"))
-            }
-
+    var database: AppDatabase = AppDatabase()
+    private var ref: DatabaseReference
+    init {
+        ref = database.getReference()
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun addTraining():Training? {
+        val key = ref.child("trainings").push().key ?: return null
+        var training = Training(key,LocalDateTime.now(),0.0,"", "run")
+        ref.child("trainings").child(key).setValue(training)
+        //addUserWithTraining(key)
+        return training
+    }
+
+    private fun addUserWithTraining(key: String?) {
+        ref.child("userWithTrainings").child("0").setValue(key)
+    }
+    fun addCoordinateToTraining(key: String?, coordinate: Coordinate){
+        if(key!=null){
+            val coordinateKey = ref.child("trainings").push().key ?: return
+            ref.child("trainings").child(key).child("coordinates").child(coordinateKey).setValue(coordinate)
+        }
+    }
+
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun setAttributesTraining(training: Training?, text: CharSequence?, distance: Double) {
+        if(training != null){
+            ref.child("trainings").child(training.trainingId).child("time").setValue(text)
+            ref.child("trainings").child(training.trainingId).child("distance").setValue(distance)
+        }
+    }
+
 }
